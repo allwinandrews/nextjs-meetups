695b31a642dc9a70544f63be2522cd0fc06715f9
Andrews <allwinandrews@gmail.com>
2021-07-13 20:16:10 +0530

    Uses getStaticPaths to prepare paths and works with fallback pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -10,3 +10,33 @@ export default function MeetupDetails() {
     />
   );
 }
+
+export async function getStaticPaths() {
+  return {
+    // fallback false only when all the data is loaded
+    fallback: false,
+    paths: [{ params: { meetupId: "m1" } }, { params: { meetupId: "m2" } }],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  // only executes in developer server, not in browser console
+  //   console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/View_to_Sharpitor_from_Meavy.JPG/1280px-View_to_Sharpitor_from_Meavy.JPG",
+        id: meetupId,
+        title: "A 1st meetup",
+        description: "Meetup Description",
+        address: "Some street",
+      },
+    },
+  };
+}
