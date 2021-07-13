3e760b5fef6459e5c04706728e67f1fa577ea862
Andrews <allwinandrews@gmail.com>
2021-07-13 17:22:10 +0530

    Navigates from '/' to /id'

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/View_to_Sharpitor_from_Meavy.JPG/1280px-View_to_Sharpitor_from_Meavy.JPG"
+      title="A 1st meetup"
+      description="Meetup Description"
+      address="Some street"
+    />
+  );
+}
